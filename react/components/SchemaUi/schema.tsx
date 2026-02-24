679e24a33d6fd3d767fb1007a759b5b441823747
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2026-02-24 18:34:29 -0300

    fix(schema): adjust schema

--- react/components/SchemaUi/schema.tsx
+++ react/components/SchemaUi/schema.tsx
@@ -51,12 +51,281 @@ const SchemaUiItemProps = {
     },
   },
   properties: {
-    classeItem: {
-      type: 'string',
-      title: 'Teste de Classe',
+    fieldNull: {
+      title: "Texto com type Null",
+      type: "null",
+      description: "Campos nulos como este são ótimos para adicionar informações extras",
       widget: {
+        'ui:field': ({ schema }: { schema: any }) => {
+          return <h4 className="ma0 f5 near-black">{schema?.description}</h4>;
+        },
+      },
+    },
+    activeItem: {
+      title: 'Ativar Item',
+      type: 'boolean',
+      default: true,
+    },
+    textProps: {
+      title: 'Text Props',
+      type: 'object',
+      widget: {
+        classNames: 'bg-black-20 ba b--light-gray ph3 mb3',
+      },
+      properties: {
+        __blockItemTitle: {
+          type: 'null',
+          title: 'Configuração do schema Relacionados a string',
+          description: 'https://github.com/rjsf-team/react-jsonschema-form',
+          widget: {
+            'ui:field': ({ schema }: { schema: any }) => {
+              return <h4 className="ma0 f5 near-black">{schema?.title}</h4>;
+            },
+          },
+        },
+        titleItem: {
+          type: 'string',
+          title: 'Título PlaceHolder',
+          description: 'Insira o texto que será exibido no botão.',
+          minLength: 10,
+          widget: {
+            'ui:widget': 'text',
+            'ui:placeholder': 'Ex.: Teste',
+          },
+        },
+        passwordItem: {
+          type: 'string',
+          title: 'Senha',
+          widget: {
+            'ui:inputType': 'password',
+            "ui:help": "Dica: Faça com que seja forte!"
+          },
+        },
+        descriptionItem: {
+          type: "string",
+          title: "Bio",
+          widget: {
+            "ui:widget": "textarea",
+            "ui:options": {
+              "rows": 5,
+              "maxLength": 10,
+            }
+          }
+        },
+      }
+    },
+    dataProps: {
+      title: 'Data Props',
+      type: 'object',
+      widget: {
+        classNames: 'bg-light-blue ba b--light-gray ph3 mb3',
+      },
+      properties: {
+        __blockItemTitle: {
+          type: 'null',
+          title: 'Configuração do schema Relacionados a data',
+          description: 'https://github.com/rjsf-team/react-jsonschema-form',
+          widget: {
+            'ui:field': ({ schema }: { schema: any }) => {
+              return <h4 className="ma0 f5 near-black">{schema?.title}</h4>;
+            },
+          },
+        },
+        dataTimeItem: {
+          type: 'string',
+          title: 'Data Time',
+          format: 'date-time',
+          widget: {
+            'ui:widget': 'datetime',
+          },
+        },
+        dataItem: {
+          type: 'string',
+          title: 'Data',
+          format: 'date',
+          widget: {
+            'ui:widget': 'date',
+          },
+        },
+        altDatetime: {
+          type: "string",
+          format: "date-time",
+          widget: {
+            "ui:widget": "alt-datetime",
+            "ui:options": {
+              "yearsRange": [
+                2023,
+                2030
+              ],
+              "format": "MDY"
+            },
+          },
+        },
+        altDate: {
+          type: "string",
+          format: "date",
+          widget: {
+            "ui:widget": "alt-date",
+            "ui:options": {
+              "yearsRange": [
+                2023,
+                2030
+              ],
+              "format": "MDY"
+            },
+          },
+        },
+      }
+    },
+    selectProps: {
+      title: 'Select Props',
+      type: 'object',
+      widget: {
+        classNames: 'bg-silver ba b--light-gray ph3 mb3',
+      },
+      properties: {
+        __blockItemTitle: {
+          type: 'null',
+          title: 'Configuração do schema Relacionados a seleções e referencias',
+          description: 'https://github.com/rjsf-team/react-jsonschema-form',
+          widget: {
+            'ui:field': ({ schema }: { schema: any }) => {
+              return <h4 className="ma0 f5 near-black">{schema?.title}</h4>;
+            },
+          },
+        },
+        selectItem: {
+          title: 'Seleção de Cor',
+          type: 'string',
+          default: '#000',
+          enumNames: ['Preto', 'Branco'],
+          enum: ['#000', '#fff'],
+          description: 'Por default a cor vem "Preto"',
+          widget: {
+            'ui:widget': 'select'
+          }
+        },
+        colorItem: {
+          title: 'Cor do titulo',
+          type: 'string',
+          widget: {
+            'ui:widget': 'color',
+          },
+          default: '#666',
+          description: 'Selecione a cor do texto principal do bloco',
+        },
+        radioItem: {
+          title: 'Escolha uma opção radio',
+          type: 'string',
+          enum: ['Opção 1', 'Opção 2', 'Opção 3'],
+          widget: {
+            'ui:widget': 'radio'
+          }
+        },
+        reference: {
+          type: 'string',
+          title: 'Seleção usando definitions como referencia',
+          $ref: "#/definitions/largeEnum"
+        },
+      }
+    },
+    restrictProps: {
+      title: 'Restrict Props',
+      type: 'object',
+      widget: {
+        classNames: 'bg-washed-red ba b--light-gray ph3 mb3',
+      },
+      properties: {
+        __blockItemTitle: {
+          type: 'null',
+          title: 'Configuração do schema Relacionados a campos restritos',
+          description: 'https://github.com/rjsf-team/react-jsonschema-form',
+          widget: {
+            'ui:field': ({ schema }: { schema: any }) => {
+              return <h4 className="ma0 f5 near-black">{schema?.title}</h4>;
+            },
+          },
+        },
+        secret: {
+          type: "string",
+          default: "Invisivel string.",
+          widget: {
+            "ui:widget": "hidden"
+          },
+        },
+        disabledItem: {
+          type: 'string',
+          title: 'Campo Desabilitado',
+          widget: {
+            'ui:disabled': true,
+          },
+          default: 'desabilitado',
+        },
+        readonlyItem: {
+          type: 'string',
+          title: 'Campo somente Leitura',
+          widget: {
+            'ui:readonly': true,
+          },
+          default: 'somenteLeitura',
+        },
+        readonlyItem2: {
+          type: 'string',
+          title: 'Campo somente Leitura 2',
+          readOnly: true,
+          default: 'somenteLeitura2',
+        },
+      }
+    },
+    arrayItem: {
+      type: 'array',
+      title: "Array de Itens",
+      maxItems: 5, // máximo de itens que podem ser cadastrados em um array
+      widget: {
+        classNames: 'bg-washed-yellow ba b--light-gray ph3 mb3',
+        'ui:options': {
+          addable: true, // False impede de adicionar Itens
+          removable: false, // false impede de apagar o item
+        }
+      },
+      items: {
+        type: 'object',
+        title: "Item Array",
+        widget: {
+          classNames: 'bg-washed-yellow ba b--light-gray ph3 mb3'
+        },
+        properties: {
+          __editorItemTitle: {
+            title: 'Identificação do Item',
+            default: 'Item',
+            description: 'Max 15 caracteres',
+            type: 'string',
+          },
+          title: {
+            type: 'string',
+            title: 'Titulo Teste',
+            widget: {
+              'ui:placeholder': 'Ex.: Teste',
+            },
+            description: '**Preenchimento Obrigatório**',
+          },
+        },
+      },
+    },
+    uploadItem: {
+      type: 'string',
+      title: 'Upload de Imagem',
+      widget: {
+        'ui:widget': 'image-uploader',
         classNames: 'bg-white-30 ba b--light-gray ph3 mb3',
       },
+    },
+    uploadFile: {
+      type: 'string',
+      title: 'Envie um arquivo',
+      widget: {
+        'ui:widget': 'file'
+      }
     },
     showMoreConfig: {
       title: 'Mostrar mais Configurações com dependencies',
