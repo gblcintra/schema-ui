6bbec2aaf2fd426b811959e9649e7e4dcfa181e2
Gabriel Cintra <gabrieloliveiracintraa@hotmail.com>
2026-03-06 01:16:16 -0300

    feat(Pokemon): add loading first info

--- react/components/SchemaUiPokemon/typing.ts
+++ react/components/SchemaUiPokemon/typing.ts
@@ -2,6 +2,7 @@ import type { Pokemon } from "pokenode-ts"
 export interface WidgetCustomSelect {
   selected?: string
   inFoPoke?: Pokemon
+  loading: boolean
 }
 export interface PropsPokemon {
   activeItem: boolean
